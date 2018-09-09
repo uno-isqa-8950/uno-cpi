e91397430328c714fa1d68f29f60909ae578c348
Goutham2591 <gviswanathan@unomaha.edu>
2018-09-08 19:19:25 -0500

    Minor changes on UI

--- cap_2018/UnoCPI/home/urls.py
+++ cap_2018/UnoCPI/home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 from . import views
+app_name ='home'
 
 urlpatterns = [
-   path('', views.home, name='base'),
-
+   url(r'^$', views.home, name='home'),
+   url(r'^home/$', views.home, name='home'),
 ]

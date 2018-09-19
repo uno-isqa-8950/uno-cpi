c62a5a468b049b18813761b94ab011aaeca322c5
Arti MUKATI <amukati@unomaha.edu>
2018-09-19 00:55:36 -0500

    Revert "Login logout"

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import CommunityPartner, Contact, CommunityType, Project, ProjectMission, MissionArea,CommunityPartnerMission,Address,CampusPartner
+from .models import CommunityPartner, Contact, CommunityType, Project, ProjectMission, Missionarea,CPMission
+
+
 
 admin.site.register(CommunityPartner)
-admin.site.register(Contact)
-admin.site.register(CommunityType)
-admin.site.register(Project)
+admin.site.register(Contact )
+admin.site.register(CommunityType )
+admin.site.register(Project )
 admin.site.register(ProjectMission )
-admin.site.register(CommunityPartnerMission)
-admin.site.register(MissionArea)
-admin.site.register(Address)
-admin.site.register(CampusPartner)
+admin.site.register(CPMission)
+admin.site.register(Missionarea)
--- home/models.py
+++ home/models.py
@@ -1,21 +1,81 @@
 from django.db import models
-from partners.models import CommunityPartner, CommunityType, CommunityPartnerMission, CampusPartner
-from projects.models import Project,ProjectMission
+from django.utils import timezone
 from django.core.validators import MinLengthValidator
 from django.core.validators import MaxLengthValidator
 from django.core.validators import RegexValidator
+
+
+class CommunityPartner (models.Model):
+    TRUE_FALSE_CHOICES = (
+        ('True', 'Yes'),
+        ('False', 'No'),
+    )
+
+    name = models.CharField(max_length= 100)
+    address_line1= models.CharField(max_length=50)
+    address_line2= models.CharField(max_length=50 ,blank= True)
+    country= models.CharField(max_length=55)
+    city= models.CharField(max_length=25)
+    state= models.CharField(max_length=15)
+    Zip = models.CharField(max_length=10)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    website_url = models.TextField()
+    college = models.CharField(max_length=50)
+    k12_level =  models.CharField(max_length=20                                                                     )
+    department = models.CharField(max_length=30)
+    active = models.BooleanField(default= True)
+    weitz_cec_part = models.CharField(max_length=6 , choices= TRUE_FALSE_CHOICES, default= False )
+
+
+    def __str__(self):
+        return str(self. name)
+
+
+class CommunityType (models.Model):
+    partner_name = models.ForeignKey(CommunityPartner, on_delete=models.CASCADE)
+    community_type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.partner_name)
+
+    def __str__(self):
+        return str(self.community_type)
+
+
+class Project (models.Model):
+    name = models.CharField(max_length=100)
+    engagement_type = models.CharField(max_length=20)
+    activity_type = models.CharField(max_length=20)
+    facilitator = models.CharField(max_length=20)
+    description = models.TextField()
+    semester = models.CharField(max_length=20)
+    no_uno_students = models.IntegerField()
+    hours_uno = models.CharField(max_length=20)
+    no_cp_members = models.IntegerField()
+    hours_cp = models.CharField(max_length=10)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(default=timezone.now)
+    details = models.CharField(max_length=100)
+    no_uno_faculty = models.IntegerField()
+    other_cp_members =models.IntegerField()
+    outcomes = models.CharField(max_length=100)
+    partners = models.ManyToManyField(CommunityPartner, related_name='p_name')
+
+    def __str__(self):
+        return str(self.name)
 
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     workphone= models.CharField(max_length=10,  validators=[MinLengthValidator(10)],
-                             help_text="Phone Number should be 10 digits",blank=True)
+                                      help_text="Phone Number should be 10 digits")
     cellphone= models.CharField(max_length=10, validators=[MinLengthValidator(10)],
-                               help_text="Phone Number should be 10 digits" , unique=True, blank=True)
+                                      help_text="Phone Number should be 10 digits" , unique=True)
     contact_type = models.CharField(max_length=15)
     email_id = models.CharField(max_length=254)
-    partner_name = models.ForeignKey('partners.CommunityPartner', on_delete=models.CASCADE)
+    partner_name = models.ForeignKey(CommunityPartner, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.first_name)
@@ -28,9 +88,9 @@ class Contact(models.Model):
         return str(self.partner_name)
 
 
-class MissionArea (models.Model):
+class Missionarea (models.Model):
     mission_code = models.CharField(max_length=10,default= 0)
-    mission_name = models.CharField(max_length=100)
+    mission_name = models.CharField(max_length=50)
     description = models.TextField()
   #  mission_code = models.ManyToManyField(CommunityPartner , through='CPMission')
 
@@ -38,18 +98,27 @@ class MissionArea (models.Model):
         return str(self.mission_name)
 
 
-class Address(models.Model):
-    address_line1 = models.CharField(max_length=1024)
-    address_line2 = models.CharField(max_length=1024, blank=True)
-    country = models.CharField(max_length=100)
-    city = models.CharField(max_length=25)
-    state = models.CharField(max_length=15)
-    Zip = models.CharField(max_length=10)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6,blank=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6,blank=True)
+class CPMission (models.Model):
+    partner_name = models.ForeignKey(CommunityPartner, on_delete=models.CASCADE)
+    mission_type = models.CharField(max_length=20)
+    mission_code = models.ForeignKey(Missionarea, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.partner_name)
 
 
+class ProjectMission (models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.CASCADE)
+    mission_type = models.CharField(max_length=20)
+    mission_code = models.ForeignKey(Missionarea, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.project_name)
+
+
+#class ProjectPartner (models.Model):
+   # project_id = models.ForeignKey(Project,  on_delete=models.CASCADE)
+    #partner_id = models.ForeignKey(CommunityPartner, on_delete=models.CASCADE)
 
 
 

43eafdaea64dfd4b205c673a1f29331841c333e4
Arti012 <amukati@unomaha.edu>
2018-09-23 14:04:06 -0500

    UI Url fixes ...

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from . import views
+#app_name = home
 
 urlpatterns = [
-   path('cpi', views.cpipage, name='cpipage'),
-   path('',views.home, name='home'),
+   path('', views.cpipage, name='cpipage'),
+   path('home',views.home, name='home'),
    path('registerCampusPartner/', views.registerCampusPartner, name='registerCampusPartner'),
    path('registerCampusPartnerUser/', views.registerCampusPartnerUser, name='registerCampusPartnerUser'),
 

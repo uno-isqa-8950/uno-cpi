e5492d6c0a32e6db13b57aa508419fc1fd132201
Arti012 <amukati@unomaha.edu>
2018-09-25 22:00:47 -0500

    heroku changes

--- UnoCPI/settings.py
+++ UnoCPI/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')z8d*5_%@v!h@bl3-vl2gn@mwcd@6vlz061+b=o02jc5@2r1gg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

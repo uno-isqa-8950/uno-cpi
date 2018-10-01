bd5fdd0bb80cd4a71c4f8859e0c642bf29bc1d72
Arti012 <amukati@unomaha.edu>
2018-09-30 20:13:17 -0500

    Model minor changes

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Project , ProjectMission,ProjectCommunityPartner, ProjectCampusPartner
+from .models import Project , ProjectMission,ProjectCommunityPartner, ProjectCampusPartner , EngagementType, ActivityType, Status
 
 admin.site.register(Project)
 admin.site.register(ProjectMission)
 admin.site.register(ProjectCommunityPartner)
 admin.site.register(ProjectCampusPartner)
+admin.site.register(EngagementType)
+admin.site.register(ActivityType)
+admin.site.register(Status)
--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Project (models.Model):
     name = models.CharField(max_length=255)
-    engagement_type = models.ForeignKey('EnagagementType',on_delete=models.CASCADE ,null=True)
+    engagement_type = models.ForeignKey('EngagementType',on_delete=models.CASCADE ,null=True)
     activity_type = models.ForeignKey('ActivityType',on_delete=models.CASCADE ,null=True)
     facilitator = models.CharField(max_length=255,blank=True)
     description = models.TextField()
@@ -14,19 +14,19 @@ class Project (models.Model):
     total_k12_students = models.IntegerField(null=True, blank=False)
     total_k12_hrs = models.CharField(max_length=10)
     total_uno_faculty = models.IntegerField(blank=True)
-    total_other_community_members =  models.IntegerField(null=False, blank=True)
+    total_other_community_members = models.IntegerField(null=False, blank=True)
     start_date = models.DateField(default=timezone.now,null=False, blank=True)
     end_date = models.DateField(default=timezone.now,null=False, blank=True)
     other_details = models.CharField(max_length=100,null=False, blank=True)
     outcomes = models.CharField(max_length=100,null=False, blank=True)
     status = models.ForeignKey('Status',on_delete=models.CASCADE,null=True)
-    total_economic_impact =models.DecimalField(max_digits=15, decimal_places=4,null=False, blank=True)
+    total_economic_impact =models.DecimalField(max_digits=15, decimal_places=4,null=True, blank=True)
     address_line1 = models.CharField(max_length=1024, blank=True)
     address_line2 = models.CharField(max_length=1024, blank=True)
     country = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=25, blank=True)
     state = models.CharField(max_length=15, blank=True)
-    Zip = models.CharField(max_length=10, blank=True)
+    zip = models.IntegerField(max_length=6,null=True,blank=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 
@@ -48,7 +48,7 @@ class ProjectMission (models.Model):
         return str(self.mission)
 
 class ProjectCommunityPartner (models.Model):
-    project  = models.ForeignKey('projects.Project',  on_delete=models.CASCADE)
+    project= models.ForeignKey('projects.Project',  on_delete=models.CASCADE)
     community_partner = models.ForeignKey('partners.CommunityPartner', on_delete=models.CASCADE)
     no_hours = models.IntegerField(blank=True,null=True)
     no_people = models.IntegerField(blank=True,null=True)
@@ -62,7 +62,7 @@ class ProjectCommunityPartner (models.Model):
 
 
 class ProjectCampusPartner (models.Model):
-    project  = models.ForeignKey('projects.Project',  on_delete=models.CASCADE)
+    project = models.ForeignKey('projects.Project',  on_delete=models.CASCADE)
     campus_partner = models.ForeignKey('partners.CampusPartner', on_delete=models.CASCADE)
     no_hours = models.IntegerField(blank=True,null=True)
     no_people = models.IntegerField(blank=True,null=True)
@@ -71,6 +71,7 @@ class ProjectCampusPartner (models.Model):
 
     def __str__(self):
         return str(self.project)
+
 
     def __str__(self):
         return str(self.campus_partner)
@@ -84,7 +85,7 @@ class Status(models.Model):
         return str(self.name)
 
 
-class EnagagementType(models.Model):
+class EngagementType(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description =models.CharField(max_length=255)
 

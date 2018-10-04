71cd999fd11b2bcbcb0fcabfed43908a8544ff81
Arti012 <amukati@unomaha.edu>
2018-10-04 10:47:51 -0500

    community partner contact

--- home/forms.py
+++ home/forms.py
@@ -99,10 +99,10 @@ class CommunityPartnerForm(forms.ModelForm):
 
                    }
         def clean_website_url(self):
-        data = self.cleaned_data.get('website_url')
-        if  not  data.startswith ('https://'):
-            raise forms.ValidationError('Url should start from "https://".')
-        return data
+            data = self.cleaned_data.get('website_url')
+            if  not  data.startswith ('https://'):
+                raise forms.ValidationError('Url should start from "https://".')
+            return data
 
 
 class CommunityContactForm(forms.ModelForm):
@@ -154,5 +154,6 @@ class CampusForm(ModelForm):
             ('False', 'No'),
         )
         weitz_cec_part = forms.ChoiceField(widget=forms.Select(choices=TRUE_FALSE_CHOICES))
+
 
 

1e77b07a38c76c02c1510e2fabae3a91e3af6bce
Arti MUKATI <amukati@unomaha.edu>
2018-10-04 15:20:05 -0500

    Revert "dry-run"

--- UnoCPI/settings.py
+++ UnoCPI/settings.py
@@ -142,9 +142,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (

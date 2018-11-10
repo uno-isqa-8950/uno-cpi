34ca0996fedfba32a846998e233b0cfb5a92ff79
Goutham2591 <gviswanathan@unomaha.edu>
2018-11-10 01:43:35 -0600

    Sidebar added to Projects Map

--- UnoCPI/settings.py
+++ UnoCPI/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'UnoCPI.wsgi.application'
 # Database
 
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -93,6 +93,7 @@ DATABASES = {
 }
 
 """
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -103,7 +104,7 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-"""
+
 
 
 ##Extending the user model to home

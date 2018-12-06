171c05535e9e787e5cca0a6b7cdcfb9b8d353fc3
Goutham2591 <gviswanathan@unomaha.edu>
2018-12-06 01:43:41 -0600

    select2 layout fix on all reports

--- UnoCPI/settings.py
+++ UnoCPI/settings.py
@@ -97,17 +97,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'UIfix1',
-#         'USER': 'postgres',
-#         'PASSWORD': 'jan@2018',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#
-# }
-# }
+
 
 
 ##Extending the user model to home

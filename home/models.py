a34686ffc9712ef4c4a67a8c1b629fdd42ca0a34
Jerrod Anzalone <aanzalone@unomaha.edu>
2019-02-17 15:13:46 -0600

    Adding basic Wagtail CMS functionality

--- home/models.py
+++ home/models.py
@@ -4,7 +4,20 @@ from django.core.validators import MinLengthValidator
 from django.core.validators import MaxLengthValidator
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+
+    class Meta:
+        verbose_name = "homepage"
 
 class UserManager(BaseUserManager):
     """Define a model manager for User model with no username field."""

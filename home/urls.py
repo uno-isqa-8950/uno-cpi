e74b4bb25b8734721a65913657cef01f8112d9e9
Vivek Bhat <vbhathosmat@unomaha.edu>
2019-02-25 10:41:36 -0600

    google maps implementaiton

--- home/urls.py
+++ home/urls.py
@@ -33,8 +33,10 @@ urlpatterns = [
    path('AdminHome/', views.AdminHome , name= 'adminhome'),
    path('Adminframe/', views.Adminframe, name='Adminframe'),
    path('Contactus',views.Contactus,name='Contactus'),
-   path('thanks/', views.thanks, name='thanks')
-
+   path('thanks/', views.thanks, name='thanks'),
+   path('googleprojectmap',views.googleprojectdata, name='googleprojectmap'),
+   path('googleDistrictmap',views.googleDistrictdata, name='googleDistrictmap'),
+   path('googlehomepage',views.googlepartnerdata, name='googlehomepage')
 
 
 ]

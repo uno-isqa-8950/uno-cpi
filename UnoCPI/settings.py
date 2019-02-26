c3cd70c5c78dced81cb17b461b1465c5af9a472f
Jerrod Anzalone <aanzalone@unomaha.edu>
2019-02-26 12:38:41 -0600

    Updating to add Amazon S3 Bucket

--- UnoCPI/settings.py
+++ UnoCPI/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
     'modelcluster',
     'taggit',
+    'storages',
     ]
 
 
@@ -188,9 +189,10 @@ LOGIN_REDIRECT_URL = reverse_lazy('/')
 LOGIN_URL = reverse_lazy('login')
 #LOGOUT_REDIRECT_URL = reverse_lazy('logout')
 
+
 ## Sample Import files download source location, DONOT DELETE
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+## MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+## MEDIA_URL = '/media/'
 
 ##
 EMAIL_HOST = 'smtp.gmail.com'
@@ -212,3 +214,13 @@ except ImportError:
     pass
 
 WAGTAIL_SITE_NAME = 'UNO-CPI'
+
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+

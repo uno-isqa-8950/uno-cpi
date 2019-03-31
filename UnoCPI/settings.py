671e172f157ee9df68922271f25b946e095ecf2a
Jerrod Anzalone <aanzalone@unomaha.edu>
2019-03-30 19:38:35 -0500

    Adding admin module to track changes

--- UnoCPI/settings.py
+++ UnoCPI/settings.py
@@ -198,8 +198,8 @@ LOGIN_URL = reverse_lazy('login')
 
 ##
 EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_HOST_USER = 'postmaster@uno-cpi.com'
-EMAIL_HOST_PASSWORD = 'e3cd9bfaca7a187b66294745ed788798-e51d0a44-d5ebd7c2'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 

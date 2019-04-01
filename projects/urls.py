35e4ee83bab797722e4fb989d24b4da1b07e78b8
Jantz021991 <djantz@unomaha.edu>
2019-03-31 20:20:58 -0500

    refactoring Projects

--- projects/urls.py
+++ projects/urls.py
@@ -5,15 +5,11 @@ from django.conf.urls import url
 # app_name = 'projects'
 
 urlpatterns = [
-    # path('communitypartnerhome/', views.communitypartnerhome, name='communitypartnerhome'),
     path('communitypartnerproject/', views.communitypartnerproject, name='communitypartnerproject'),
-    #path('communitypartnerprojectedit/<int:pk>/',views.communitypartnerprojectedit, name='communitypartnerprojectedit'),
-
-    #url(r'^project/$', views.project_list, name='project_list'),
     url(r'^project/(?P<pk>\d+)/edit/$', views.editProject, name='editProject'),
     url(r'^createProject/$', views.createProject, name='createProject'),
     path('myProjects/',views.myProjects, name='myProjects'),
-    url(r'^allProjects/', views.showAllProjects, name='showAllProjects'),
+    path('allProjects/', views.showAllProjects, name='showAllProjects'),
     url(r'^projectSearchAdd/(?P<pk>\d+)/', views.SearchForProjectAdd, name='projectSearchAdd'),
     path('projectspublicreport/', views.projectsPublicReport, name='projectspublicreport'),
     path('communitypublicreport/', views.communityPublicReport, name='communitypublicreport'),

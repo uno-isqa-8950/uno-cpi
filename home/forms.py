6d424aa241b1df5a0c157caf4a4d01ccaa762bce
Jantz021991 <djantz@unomaha.edu>
2019-04-13 20:48:38 -0500

    Validation check for Campus Partner User

--- home/forms.py
+++ home/forms.py
@@ -74,8 +74,8 @@ class CampususerForm(forms.ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data['email']
-        if "edu" != email.split("@")[1].split('.')[1]:
-            raise forms.ValidationError("Please use your campus email (.edu) for the registration of a campus partner")
+        if ".edu" in email:
+            raise forms.ValidationError("Please use your campus email (.edu) for the registration of a Campus Partner")
         return email
 
     def clean_password2(self):

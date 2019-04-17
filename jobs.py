e52bc77d9a7249f40fcfb209231f5986321ea4d4
edosseh <edemdosseh@gmail.com>
2019-04-17 01:15:41 -0500

    Commit test scripts for 1065 and 1130 and update to sqlfiles

--- jobs.py
+++ jobs.py
@@ -42,42 +42,29 @@ def scheduled_job():
         # create a temp table with all projects start and end dates
         cursor.execute(sql.start_and_end_dates_temp_table_sql)
 
-        # fetch all community partners to be set to inactive
-        # cursor.execute(sql.comm_partners_to_be_set_to_inactive)
-        #
-        # inactive_comm_partners = cursor.fetchall()
-        # print("Here is the list of all projects to be set to inactive", "\n")
-        # loop to print all the data
-        # for i in inactive_comm_partners:
-        #     print(i)
-
-        # fetch all community partners to be set to active
-        # cursor.execute(sql.comm_partners_to_be_set_to_active)
-        #
-        # active_comm_partners = cursor.fetchall()
-        # print("Here is the list of all projects to be set to active", "\n")
-        # loop to print all the data
-        # for i in active_comm_partners:
-        #     print(i)
-
         # UPDATE PROJECT STATUS TO ACTIVE
         cursor.execute(sql.update_project_to_active_sql)
+        connection.commit()
 
         # UPDATE PROJECT STATUS TO COMPLETED
         cursor.execute(sql.update_project_to_inactive_sql)
+        connection.commit()
 
-        # UPDATE PROJECT STATUS TO COMPLETED
+        # UPDATE PROJECT STATUS TO PENDING
         cursor.execute(sql.update_project_to_pending_sql)
+        connection.commit()
 
         # UPDATE COMMUNITY PARTNER WHEN TIED TO A INACTIVE PROJECTS ONLY TO FALSE(INACTIVE)
         cursor.execute(sql.update_comm_partner_to_inactive_sql)
+        connection.commit()
 
         # UPDATE  COMMUNITY PARTNER WHEN TIED TO A BOTH ACTIVE
         # and / or INACTIVE or JUST ACTIVE PROJECTS ONLY TO TRUE(ACTIVE)
         cursor.execute(sql.update_comm_partner_to_active_sql)
+        connection.commit()
 
         # drop all_projects_start_and_end_date temp table
-        cursor.execute(sql.drop_temp_table_all_projects_start_and_end_dates_sql)
+        # cursor.execute(sql.drop_temp_table_all_projects_start_and_end_dates_sql)
 
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to Postgres SQL", error)
@@ -85,8 +72,11 @@ def scheduled_job():
         # closing database connection.
         if connection:
             connection.commit()
+            # drop all_projects_start_and_end_date temp table
+            cursor.execute(sql.drop_temp_table_all_projects_start_and_end_dates_sql)
             cursor.close()
             connection.close()
             print("Postgres SQL connection is closed")
+
 
 sched.start()

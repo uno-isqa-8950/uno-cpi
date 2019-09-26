535f9b4e60172e99159d14dddfeb3864f5aacf42
rconaway-uno <41027252+rconaway-uno@users.noreply.github.com>
2019-09-26 17:53:46 -0500

    Ron K12 Filter Final Changes

--- projects/forms.py
+++ projects/forms.py
@@ -10,7 +10,7 @@ from django.forms import ModelForm
 
 
 K12_CHOICES = [
-    ('Yes', 'K12 Project'), ('No', 'Not a K12 Project')]
+    ('All', 'All Projects'), ('Yes', 'K12 Project'), ('No', 'Not a K12 Project')]
 
 
 class K12ChoiceForm(forms.Form):

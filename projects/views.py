87057381be16b6f47a7781e89b62f19e4cc47a41
Siri Chandana <siri.goparaju@gmail.com>
2019-10-05 21:42:11 -0500

    #1390 - Search Improvements in projects
    Added Changes required for search improvements in projects

--- projects/views.py
+++ projects/views.py
@@ -906,8 +906,11 @@ def checkProject(request):
         project = object.project_name.split('(')[0]
         for part in ProjectCommunityPartner.objects.filter(project_name__project_name__exact=object.project_name):
             compartner = part.community_partner
+            # Sprint2-#1390- Added Capus Partner list- Search Improvements
+        for part in ProjectCampusPartner.objects.filter(project_name__project_name__exact=object.project_name):
+            campartner = part.campus_partner
+            combinedList = [object.project_name.split('(')[0], str(compartner), str(campartner)]
 
-            combinedList = [object.project_name.split('(')[0],str(compartner)]
             if combinedList not in projectNames:
                 projectNames.append(combinedList)
 

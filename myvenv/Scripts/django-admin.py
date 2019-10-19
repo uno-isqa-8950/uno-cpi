82d70bb2448b7236dd299b46ae4bdaa54a8189e6
nochuba <nochuba@unomaha.edu>
2019-10-19 13:16:40 -0500

    sprint 3 changes

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\nochu\desktop\capstone_sprint2\uno-cpi\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

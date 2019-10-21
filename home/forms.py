4f83d5b42a134b76c4389094a70334f80c9cc82e
wcobb <wcobb@unomaha.edu>
2019-10-20 23:14:58 -0500

    Changed forms to add the state codes for dropdown to use in forms.  Also updated the red for the clickable to the correct red according to client.
    The files changed were uno-cpi/home/templates/reports/ engagementTypeReport.html and ProjectPartnerInfo and uno-cpi/forms.

--- home/forms.py
+++ home/forms.py
@@ -14,6 +14,24 @@ from home.models import Contact, MissionArea, HouseholdIncome
 from projects.models import Project, EngagementType, ActivityType, Status, ProjectCampusPartner, \
     ProjectCommunityPartner, ProjectMission, AcademicYear
 
+STATE_CHOICES = [
+    (('AL', 'Alabama'), ('AZ', 'Arizona'), ('AR', 'Arkansas'), ('CA', 'California'),
+     ('CO', 'Colorado'), ('CT', 'Connecticut'), ('DE', 'Delaware'),
+     ('DC', 'District of Columbia'), ('FL', 'Florida'), ('GA', 'Georgia'), ('ID', 'Idaho'),
+     ('IL', 'Illinois'), ('IN', 'Indiana'), ('IA', 'Iowa'), ('KS', 'Kansas'),
+     ('KY', 'Kentucky'), ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'),
+     ('MA', 'Massachusetts'), ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MS', 'Mississippi'),
+     ('MO', 'Missouri'), ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'),
+     ('NH', 'New Hampshire'), ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NY', 'New York'),
+     ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('OH', 'Ohio'), ('OK', 'Oklahoma'),
+     ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'),
+     ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'),
+     ('VT', 'Vermont'), ('VA', 'Virginia'), ('WA', 'Washington'), ('WV', 'West Virginia'),
+     ('WI', 'Wisconsin'), ('WY', 'Wyoming'))]
+
+
+class StateChoiceForm(forms.Form):
+    state_choice = forms.ChoiceField(label="State Choices", choices=STATE_CHOICES, required=False)
 
 class CampusPartnerUserForm(forms.ModelForm):
 

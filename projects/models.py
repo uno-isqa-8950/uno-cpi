20ad5934cdf4a8f50d0e9f158d99d32c1a571740
dmishra2904 <Div@knp91>
2019-10-30 19:06:59 -0500

    Updated Projects model to make other_subcat as list array value

--- projects/models.py
+++ projects/models.py
@@ -100,7 +100,7 @@ class Project (models.Model):
     updated_date = models.DateTimeField(auto_now_add=True)
     campus_lead_staff = ArrayField(base_field=models.CharField(max_length=100), size=10, blank=True, null=True)
     project_type = models.CharField(max_length=20, choices=project_choices, default='Project')
-    other_sub_category = models.CharField(max_length=1000, null=True, blank=True)
+    other_sub_category = ArrayField(base_field=models.CharField(max_length=100), size=10, blank=True, null=True)
     recursive_project = models.BooleanField(default=False)
     history = HistoricalRecords()
 

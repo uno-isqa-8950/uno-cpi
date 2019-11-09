e22cfbb7941eb392087b8d447a16fd579525440a
Aysha Ruhi Damda <adamda@unomaha.edu>
2019-11-09 12:54:05 -0600

    conn

--- charts_campuspartner_json.py
+++ charts_campuspartner_json.py
@@ -9,14 +9,12 @@ from UnoCPI import settings
 
 currentDT = datetime.datetime.now()
 logger=logging.getLogger("UNO CPI Application")
-conn = psycopg2.connect("dbname = 'UNO_CPI_DEV' user = 'postgres' host = 'localhost' password = 'horcrux'")
-# conn =   psycopg2.connect(user=settings.DATABASES['default']['USER'],
-#                               password=settings.DATABASES['default']['PASSWORD'],
-#                               host=settings.DATABASES['default']['HOST'],
-#                               port=settings.DATABASES['default']['PORT'],
-#                               database=settings.DATABASES['default']['NAME'],
-#                               sslmode="require")
-
+conn =   psycopg2.connect(user=settings.DATABASES['default']['USER'],
+                              password=settings.DATABASES['default']['PASSWORD'],
+                              host=settings.DATABASES['default']['HOST'],
+                              port=settings.DATABASES['default']['PORT'],
+                              database=settings.DATABASES['default']['NAME'],
+                              sslmode="require")
 if (conn):
     logger.info("Connection Successful!")
 else:

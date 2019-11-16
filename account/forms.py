4a8df5e751a9b2586246042a4b8748344a0304ba
divyakorrapati <dkorrapati@unomaha.edu>
2019-11-16 12:53:10 -0600

    Commit for alignment in password reset form Files changed: account/forms.py account/templates/registration/password_reset_form.html

--- account/forms.py
+++ account/forms.py
@@ -25,6 +25,6 @@ class EmailValidationOnForgotPassword(PasswordResetForm):
     def clean_email(self):
         email = self.cleaned_data['email']
         if not User.objects.filter(email__iexact=email, is_active=True).exists():
-            msg = _("Please enter a valid Email address.There is no user registered with the specified E-Mail address.")
+            msg = ("Please enter a valid Email address.There is no user registered with the specified Email address.")
             self.add_error('email', msg)
         return email

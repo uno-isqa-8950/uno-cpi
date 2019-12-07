b57105409ac6a18773216f8a0e05b1abd1456994
babonaresh <nbashaboena@unomaha.edu>
2019-12-07 11:44:25 -0600

    updates for network chart and focus area

--- home/static/js/network.js
+++ home/static/js/network.js
@@ -131,10 +131,10 @@ function  getchartdata(Missionarea,Collegenames,campus_partner_json,community_pa
         feature["campus_partner_ids"].forEach(item => projectCampus.add(item));
     })
 
-    if (!not_set.includes(engagement_type) || !not_set.includes(academic_year) || !not_set.includes(college_name) || !not_set.includes(campus_partner) || ['CURR_CAMP', 'FORMER_CAMP'].includes(weitz_cec_part)) {
+    if (!not_set.includes(engagement_type) || !not_set.includes(academic_year) || !not_set.includes(college_name) || !not_set.includes(campus_partner) || !not_set.includes(legislative) || ['CURR_CAMP', 'FORMER_CAMP'].includes(weitz_cec_part)) {
         var community_partner_json = community_partner_json.filter(d => projectCommunities.has(d.community_partner_id));
     }
-    if (!not_set.includes(engagement_type) || !not_set.includes(academic_year) || !not_set.includes(comm_type) || !not_set.includes(legislative) || ['CURR_COMM', 'FORMER_COMM'].includes(weitz_cec_part)) {
+    if (!not_set.includes(engagement_type) || !not_set.includes(academic_year) || !not_set.includes(comm_type)  || ['CURR_COMM', 'FORMER_COMM'].includes(weitz_cec_part)) {
         var campus_partner_json = campus_partner_json.filter(d => projectCampus.has(d.campus_partner_id));
     }
 

7d02b7b9075ca1a465cc8f2bbd62853c926d6efd
DoctorEww <dgriess084@gmail.com>
2022-10-26 13:36:22 -0500

    removed unused imports

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout,get_user_model
 from .forms import LoginForm
 from django.contrib import messages

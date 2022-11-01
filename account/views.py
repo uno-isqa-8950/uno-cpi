c7aba77b4b707bd8dff6863d733ce03edb2bde1b
snavelycoreyt <csnavely@unomaha.edu>
2022-10-31 20:28:26 -0500

    Update views.py

--- account/views.py
+++ account/views.py
@@ -95,7 +95,7 @@ def user_login(request):
                     return response
                 elif user.is_superuser:
                     login(request, user)
-                    response = redirect('/Admin-Home')
+                    response = redirect('/Admin-frame')
                     return response
             else:
                 messages.error(request, 'Email or Password is incorrect or contact system administration.')
@@ -141,7 +141,7 @@ def redirectUNOUser(request, key):
                 return response
             elif user.is_superuser:
                 login(request, user)
-                response = redirect('/Admin-Home')
+                response = redirect('/Admin-frame')
                 return response
         else:
             messages.error(request,

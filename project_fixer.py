b5b2a0abe73eaea0c287f8323635da8b5eb8eeb6
Corey Snavely <70164473+snavelycoreyt@users.noreply.github.com>
2022-11-28 17:40:44 -0600

    Update project_fixer.py

--- project_fixer.py
+++ project_fixer.py
@@ -5,34 +5,48 @@ django.setup()
 from projects.models import *
 
 for projmis in ProjectMission.objects.all():
-    for proj in Project.objects.all():
-        if proj.id == projmis.project_name.id:
-            print("Match on: ")
-            print(projmis.project_name)
-            print(proj.project_name)
-            print(projmis.mission)
-            Project.objects.get(id=proj.id).mission_area.add(projmis.mission)
+    try:
+        for proj in Project.objects.all():
+            if proj.id == projmis.project_name.id:
+                print("Match on: ")
+                print(projmis.project_name)
+                print(proj.project_name)
+                print(projmis.mission)
+                    Project.objects.get(id=proj.id).mission_area.add(projmis.mission)
+    except:
+        print("An exception occurred")
+
 
 for projcamp in ProjectCampusPartner.objects.all():
-    for proj in Project.objects.all():
-        if proj.id == projcamp.project_name.id:
-            print("Match on: ")
-            print(projcamp.project_name)
-            print(proj.project_name)
-            Project.objects.get(id=proj.id).campus_partner.add(projcamp.campus_partner)
+    try: 
+        for proj in Project.objects.all():
+            if proj.id == projcamp.project_name.id:
+                print("Match on: ")
+                print(projcamp.project_name)
+                print(proj.project_name)
+                Project.objects.get(id=proj.id).campus_partner.add(projcamp.campus_partner)
+    except:
+        print("An exception occurred")
+        
 
 for projcom in ProjectCommunityPartner.objects.all():
-    for proj in Project.objects.all():
-        if proj.id == projcom.project_name.id:
-            print("Match on: ")
-            print(projcom.project_name)
-            print(proj.project_name)
-            Project.objects.get(id=proj.id).community_partner.add(projcom.community_partner)
+    try:
+        for proj in Project.objects.all():
+            if proj.id == projcom.project_name.id:
+                print("Match on: ")
+                print(projcom.project_name)
+                print(proj.project_name)
+                Project.objects.get(id=proj.id).community_partner.add(projcom.community_partner)
+    except:
+        print("An exception occurred")
 
 for projsub in ProjectSubCategory.objects.all():
-    for proj in Project.objects.all():
-        if proj.id == projsub.project_name.id:
-            print("Match on: ")
-            print(projsub.project_name)
-            print(proj.project_name)
-            Project.objects.get(id=proj.id).subcategory.add(projsub.sub_category)
+    try:
+        for proj in Project.objects.all():
+            if proj.id == projsub.project_name.id:
+                print("Match on: ")
+                print(projsub.project_name)
+                print(proj.project_name)
+                Project.objects.get(id=proj.id).subcategory.add(projsub.sub_category)
+    except:
+        print("An exception occurred")      

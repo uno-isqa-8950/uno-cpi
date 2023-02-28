84fda8bcc8a678a01767037450ce430993366581
Diana <diana.monta98@gmail.com>
2023-02-27 20:03:51 -0600

    Adding test scripts

--- UnoCPI/settings.py
+++ UnoCPI/settings.py
@@ -123,10 +123,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
+        'USER': 'postgres',
         'PASSWORD':os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'PORT': 5432,
     }
 }
 

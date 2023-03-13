2c5115a1e5c5c580051823614caaa485664dd112
kvelayutham1 <89324394+kvelayutham1@users.noreply.github.com>
2023-03-13 18:54:12 -0500

    Test Scripts: SP2_Cypress scripts KV

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,21 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import * as users from '../fixtures/users.json'
+
+Cypress.Commands.add("loginCampusUser", (user) => {
+    //adding a new command named login
+   const username = users.campusUser.username
+   const password = users.campusUser.password
+    cy.get("#email_input").type(username).type('{enter}')
+    cy.get("#password_input").type(password);
+    cy.get("#btnLogin").click();
+});
+Cypress.Commands.add("loginAdminUser", (user) => {
+    //adding a new command named login
+   const username = users.adminUser.username
+   const password = users.adminUser.password
+    cy.get("#email_input").type(username).type('{enter}')
+    cy.get("#password_input").type(password);
+    cy.get("#btnLogin").click();
+});

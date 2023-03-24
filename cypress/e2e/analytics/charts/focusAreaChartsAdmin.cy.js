e9a1dc181dfed731fb76b5805a1f710364b57bdb
anguyen2396 <77310745+anguyen2396@users.noreply.github.com>
2023-03-23 21:39:10 -0500

    test scripts

--- cypress/e2e/analytics/charts/focusAreaChartsAdmin.cy.js
+++ cypress/e2e/analytics/charts/focusAreaChartsAdmin.cy.js
@@ -10,7 +10,7 @@ beforeEach(() => {
   cy.visit(Cypress.env('baseUrl'))
 })
 
-describe('Charts Focus Area', () => {
+describe('Charts Focus Area Admin User', () => {
   beforeEach(function() {
     cy.fixture("datareports").then(function(data) {
       this.data = data
@@ -22,51 +22,92 @@ describe('Charts Focus Area', () => {
   it('visits the site', function() {
     cy.visit(Cypress.env('baseUrl'))
   })
-
-  it('Analytic Charts Focus Area User', function() {
+  //Check navigation for Focus Area chart
+  it('Check Navigation', function() {
     cy.get("[data-cy='analytics']").click().should('be.visible')
-    cy.contains('Charts').next('.dropdown-menu').then($el => {
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
       cy.wrap($el).invoke('show').should('be.visible')
-      cy.wrap($el).contains('Focus Areas').click()
+      cy.wrap($el).get("[data-cy='Focus Area']").click()
+    })
+    cy.get("[data-cy='Focus Area]").should('contain.text', 'Focus Areas')
+  })
+  //Hide Filters
+  it('Hide Filters', function() {
+    cy.get("[data-cy='analytics']").click().should('be.visible')
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
+      cy.wrap($el).invoke('show').should('be.visible')
+      cy.wrap($el).get("[data-cy='Focus Area']").click()
+    })
+    cy.get('[data-cy="hide filters"]').should('have.value', 'Hide Filters')
+    cy.get('[data-cy="hide filters"]').click()
+    // check the filers are they visible
+    cy.get('[data-cy="hide filters"]').click()
+    cy.get('[data-cy="hide filters"]').should('have.value', 'Hide Filters')
+    cy.get('.select2-selection__placeholder').should('be.visible')
+  })
+
+  //Reset Filters
+  it('Reset Filters', function() {
+    cy.get("[data-cy='analytics']").click().should('be.visible')
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
+      cy.wrap($el).invoke('show').should('be.visible')
+      cy.wrap($el).get("[data-cy='Focus Area']").click()
+    })
+    cy.get('#select2-id_academicyear-container > .select2-selection__placeholder').contains('Previous Academic Year')
+    cy.get('[data-cy="academic_year"]').select(this.data.focus_area1, {force: true})
+    cy.get('#select2-id_academicyear-container').should('have.text', this.data.focus_area1)
+
+    cy.get('#select2-id_engagement_type-container > .select2-selection__placeholder').contains('All Engagement Types')
+    cy.get('[data-cy="engagement_type"]').select(this.data.engagement_type3, {force: true})
+    cy.get('#select2-id_engagement_type-container').should('have.text', this.data.engagement_type3)
+
+    cy.get('#select2-id_community_type-container > .select2-selection__placeholder').contains('All Community Organization Types')
+    cy.get('[data-cy="community_type"]').select(this.data.community_type1, {force: true})
+    cy.get('#select2-id_community_type-container').should('have.text', this.data.community_type1)
+
+    cy.get('[data-cy="reset filters"]').should('have.value', 'Reset Filters')
+    cy.get('[data-cy="reset filters"]').click()
+    // check the filers are they visible
+    cy.get('[data-cy="reset filters"]').click()
+    cy.get('[data-cy="reset filters"]').should('have.value', 'Reset Filters')
+    cy.get('.select2-selection__placeholder').should('be.visible')
+  })
+
+  //Filter options
+  it('Analytic Charts Focus Area Admin User', function() {
+    cy.get("[data-cy='analytics']").click().should('be.visible')
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
+      cy.wrap($el).invoke('show').should('be.visible')
+      cy.wrap($el).get("[data-cy='Focus Area']").click()
     })
 
-    cy.get("[data-cy='Hide_Filters']").should('not.be.disabled')
-    cy.get("[data-cy='Reset Filters']").should('not.be.disabled')
+    cy.get('#select2-id_academicyear-container > .select2-selection__placeholder').contains('Previous Academic Year')
+    cy.get('[data-cy="academic_year"]').select(this.data.focus_area1,{force:true})
+    cy.get('#select2-id_academicyear-container').should('have.text' ,this.data.focus_area1)
 
-    cy.get('#select2-id_academicyear-container').click()
-    cy.get('#select2-id_academicyear-results').then(($li) => {
-      cy.wrap($li).contains(this.data.academic_year2).should('have.text', this.data.academic_year2).click()
-    })
+    cy.get('#select2-id_engagement_type-container > .select2-selection__placeholder').contains('All Engagement Types')
+    cy.get('[data-cy="engagement_type"]').select(this.data.engagement_type3,{force:true})
+    cy.get('#select2-id_engagement_type-container').should('have.text' ,this.data.engagement_type3)
 
-    cy.get('#select2-id_engagement_type-container').click()
-    cy.get('#select2-id_engagement_type-results').then(($li) => {
-      cy.wrap($li).contains(this.data.engagement_type3).should('have.text', this.data.engagement_type3).click()
-    })
+    cy.get('#select2-id_community_type-container > .select2-selection__placeholder').contains('All Community Organization Types')
+    cy.get('[data-cy="community_type"]').select(this.data.community_type1,{force:true})
+    cy.get('#select2-id_community_type-container').should('have.text' ,this.data.community_type1)
 
-    cy.get('#select2-id_community_type-container').click()
-    cy.get('#select2-id_community_type-results').then(($li) => {
-      cy.wrap($li).contains(this.data.community_type1).should('have.text', this.data.community_type1).click()
-    })
+    cy.get('#select2-id_college_name-container > .select2-selection__placeholder').contains('All College and Main Units')
+    cy.get('[data-cy="college_name"]').select(this.data.college_name3,{force:true})
+    cy.get('#select2-id_college_name-container').should('have.text' ,this.data.college_name3)
 
-    cy.get('#select2-id_college_name-container').click()
-    cy.get('#select2-id_college_name-results').then(($li) => {
-      cy.wrap($li).contains(this.data.college_name3).should('have.text', this.data.college_name3).click()
-    })
+    cy.get('#select2-id_campus_partner-container > .select2-selection__placeholder').contains('All Campus Partner')
+    cy.get('[data-cy="campus_partner"]').select(this.data.campus_partner3,{force:true})
+    cy.get('#select2-id_campus_partner-container').should('have.text' ,this.data.campus_partner3)
 
-    cy.get('#select2-id_campus_partner-container').click()
-    cy.get('#select2-id_campus_partner-results').then(($li) => {
-      cy.wrap($li).contains(this.data.community_type6).should('have.text', this.data.community_type6).click()
-    })
-
-    cy.get('#select2-id_weitz_cec_part-container').click()
-    cy.get('#select2-id_weitz_cec_part-results').then(($li) => {
-      cy.wrap($li).contains(this.data.cec_part1).should('have.text', this.data.cec_part1).click()
-    })
+    cy.get('#select2-id_weitz_cec_part-container > .select2-selection__placeholder').contains('All (CEC/Non-CEC Partners)')
+    cy.get('[data-cy="weitz_cec_part"]').select(this.data.cec_part1,{force:true})
+    cy.get('#select2-id_weitz_cec_partr-container').should('have.text' ,this.data.cec_part1)
 
     cy.get('.highcharts-root').should('be.visible').and(chart => {
       expect(chart.height()).to.be.greaterThan(200)
     })
-
     cy.get('.highcharts-xaxis').contains('Focus Areas')
     cy.get('.highcharts-yaxis').contains('Projects/Community Partners')
     cy.get('.highcharts-xaxis-labels').filter(':contains("Arts, Culture and Humanities")').should('exist')
@@ -79,10 +120,10 @@ describe('Charts Focus Area', () => {
   })
 
   it('Chart legends', function () {
-    cy.get('#analyticnav').click()
-    cy.contains('Charts').next('.dropdown-menu').then($el => {
+    cy.get("[data-cy='analytics']").click()
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
       cy.wrap($el).invoke('show')
-      cy.wrap($el).contains('Focus Areas').click()
+      cy.wrap($el).get("[data-cy='Focus Area']").click()
     })
     cy.get('.highcharts-series-0 > text').contains("Projects").should("be.visible")
     cy.get('.highcharts-series-0 > rect').should('have.attr', 'fill', 'turquoise')
--- cypress/e2e/analytics/charts/projectAndPartnerTrendsChartsAdmin.cy.js
+++ cypress/e2e/analytics/charts/projectAndPartnerTrendsChartsAdmin.cy.js
@@ -18,69 +18,109 @@ describe('Charts Project and Partner Trend', () => {
       cy.loginAdminUser(user)
     })
   })
-
   it('visits the site', function() {
     cy.visit(Cypress.env('baseUrl'))
   })
+  //Check navigation to Project and Partner Trends charts
+  it('Check navigation', function () {
+    cy.get("[data-cy='analytics']").click()
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
+      cy.wrap($el).invoke('show')
+      cy.wrap($el).get("[data-cy='Project and Partner Trends']").click()
+    })
+      cy.get("[data-cy='Project and Partner Trends']").should('contain.text', 'Project and Partner Trends')
+  })
+  //Hide Filter
+  it('Hide Filters', function () {
+    cy.get("[data-cy='analytics']").click().should('be.visible')
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
+      cy.wrap($el).invoke('show').should('be.visible')
+      cy.wrap($el).get("[data-cy='Project and Partner Trends']").click()
+    })
+    cy.get('[data-cy="hide filters"]').should('have.value', 'Hide Filters')
+    cy.get('[data-cy="hide filters"]').click()
+    // check the filers are they visible
+    cy.get('[data-cy="hide filters"]').click()
+    cy.get('[data-cy="hide filters"]').should('have.value', 'Hide Filters')
+    cy.get('.select2-selection__placeholder').should('be.visible')
+  })
+  //Reset filters
+  it('Reset Filters', function () {
+     cy.get("[data-cy='analytics']").click()
+     cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
+         cy.wrap($el).invoke('show')
+         cy.wrap($el).get("[data-cy='Project and Partner Trends']").click()
+     })
+     cy.get('#select2-id_mission-container > .select2-selection__placeholder').contains('All Focus Area')
+     cy.get('[data-cy="mission"]').select(this.data.focus_area1,{force:true})
+     cy.get('#select2-id_mission-container').should('have.text' ,this.data.focus_area1)
 
+     cy.get('#select2-id_community_type-container > .select2-selection__placeholder').contains('All Community Organization Types')
+     cy.get('[data-cy="community_type"]').select(this.data.community_type5,{force:true})
+     cy.get('#select2-id_community_type-container').should('have.text' ,this.data.community_type5)
+
+     cy.get('#select2-id_engagement_type-container > .select2-selection__placeholder').contains('All Engagement Type')
+     cy.get('[data-cy="engagement_type"]').select(this.data.engagement_type3,{force:true})
+     cy.get('#select2-id_engagement_type-container').should('have.text' ,this.data.engagement_type3)
+
+     cy.get('[data-cy="reset filters"]').should('have.value', 'Reset Filters')
+     cy.get('[data-cy="reset filters"]').click()
+     // check the filers are they visible
+     cy.get('[data-cy="reset filters"]').click()
+     cy.get('[data-cy="reset filters"]').should('have.value', 'Reset Filters')
+     cy.get('.select2-selection__placeholder').should('be.visible')
+
+  })
+  // Filter options
   it('Analytic Charts Project and Partner Trends', function() {
     cy.get("[data-cy='analytics']").click().should('be.visible')
-    cy.contains('Charts').next("[data-cy='chartsdropdown']").then($el => {
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
       cy.wrap($el).invoke('show').should('be.visible')
-      cy.wrap($el).contains('Project and Partner Trend').click()
+      cy.wrap($el).get("[data-cy='Project and Partner Trends']").click()
     })
 
-    cy.get("[data-cy='Hide Filters']").should('not.be.disabled')
-    cy.get("[data-cy='Reset Filters']").should('not.be.disabled')
+    cy.get('#select2-id_mission-container > .select2-selection__placeholder').contains('All Focus Area')
+    cy.get('[data-cy="mission"]').select(this.data.focus_area1,{force:true})
+    cy.get('#select2-id_mission-container').should('have.text' ,this.data.focus_area1)
 
-    cy.get('#select2-id_mission-container').click()
-    cy.get('#select2-id_mission-results').then(($li) => {
-      cy.wrap($li).contains(this.data.focus_area1).should('have.text', this.data.focus_area1).click()
-    })
+    cy.get('#select2-id_community_type-container > .select2-selection__placeholder').contains('All Community Organization Types')
+    cy.get('[data-cy="community_type"]').select(this.data.community_type5,{force:true})
+    cy.get('#select2-id_community_type-container').should('have.text' ,this.data.community_type5)
 
-    cy.get('#select2-id_community_type-container').click()
-    cy.get('#select2-id_community_type-results').then(($li) => {
-      cy.wrap($li).contains(this.data.community_type6).should('have.text', this.data.community_type6).click()
-    })
+    cy.get('#select2-id_engagement_type-container > .select2-selection__placeholder').contains('All Engagement Type')
+    cy.get('[data-cy="engagement_type"]').select(this.data.engagement_type3,{force:true})
+    cy.get('#select2-id_engagement_type-container').should('have.text' ,this.data.engagement_type3)
 
-    cy.get('#select2-id_engagement_type-container').click()
-    cy.get('#select2-id_engagement_type-results').then(($li) => {
-      cy.wrap($li).contains(this.data.engagement_type3).should('have.text', this.data.engagement_type3).click()
-    })
+    cy.get('#select2-id_college_name-container > .select2-selection__placeholder').contains('All College and Main Units')
+    cy.get('[data-cy="college_name"]').select(this.data.college_name1,{force:true})
+    cy.get('#select2-id_college_name-container').should('have.text' ,this.data.college_name1)
 
-    cy.get('#select2-id_college_name-container').click()
-    cy.get('#select2-id_college_name-results').then(($li) => {
-      cy.wrap($li).contains(this.data.college_name4).should('have.text', this.data.college_name4).click()
-    })
+    cy.get('#select2-id_campus_partner-container > .select2-selection__placeholder').contains('All Campus Partner')
+    cy.get('[data-cy="campus_partner"]').select(this.data.campus_partner3,{force:true})
+    cy.get('#select2-id_campus_partner-container').should('have.text' ,this.data.campus_partner3)
 
-    cy.get('#select2-id_campus_partner-container').click()
-    cy.get('#select2-id_campus_partner-results').then(($li) => {
-      cy.wrap($li).contains(this.data.campus_partner3).should('have.text', this.data.campus_partner3).click()
-    })
-
-    cy.get('#select2-id_weitz_cec_part-container').click()
-    cy.get('#select2-id_weitz_cec_part-results').then(($li) => {
-      cy.wrap($li).contains(this.data.cec_part1).should('have.text', this.data.cec_part1).click()
-    })
+    cy.get('#select2-id_weitz_cec_part-container > .select2-selection__placeholder').contains('All (CEC/Non-CEC Partners)')
+    cy.get('[data-cy="weitz_cec_part"]').select(this.data.cec_part1,{force:true})
+    cy.get('#select2-id_weitz_cec_part-container').should('have.text' ,this.data.cec_part1)
 
     cy.get('.highcharts-root').should('be.visible').and(chart => {
       expect(chart.height()).to.be.greaterThan(200)
     })
-
     cy.get('.highcharts-yaxis').contains('Projects/Partners')
     cy.get('.highcharts-xaxis').contains('Academic Years')
   })
-
+  //Verify chart legends
   it('Chart legends', function () {
     cy.get("[data-cy='analytics']").click()
-    cy.contains('Charts').next('.dropdown-menu').then($el => {
+    cy.get("[data-cy='charts']").next("[data-cy='chartsdropdown']").then($el => {
       cy.wrap($el).invoke('show')
-      cy.wrap($el).contains('Project and Partner Trends').click()
+      cy.wrap($el).get("[data-cy='Project and Partner Trends']").click()
     })
     cy.get('.highcharts-series-0 > text').contains("Projects").should("be.visible")
+    //cy.get('.highcharts-series-0 > path').should('have.attr', 'fill', 'turquoise')
     cy.get('.highcharts-series-1 > text').contains("Community Partners").should("be.visible")
+    //cy.get('.highcharts-series-1 > path').should('have.attr', 'fill', 'teal')
     cy.get('.highcharts-series-2 > text').contains("Campus Partners").should("be.visible")
-  })
-
-
+    //cy.get('.highcharts-series-2 > path').should('have.attr', 'fill', 'blue')
+    })
 })

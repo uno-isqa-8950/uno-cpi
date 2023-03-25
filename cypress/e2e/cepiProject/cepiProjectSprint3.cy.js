e066b2562c46b0a51735615fd6eea100fe141d03
kvelayutham1 <89324394+kvelayutham1@users.noreply.github.com>
2023-03-25 16:43:35 -0500

    Removed hardcoded links from reports, home page and my draft test scripts; command.js file update to include env.json data

--- cypress/e2e/cepiProject/cepiProjectSprint3.cy.js
+++ cypress/e2e/cepiProject/cepiProjectSprint3.cy.js
@@ -1,6 +1,15 @@
-import '../homepage/homepagePublicUser.cy'
+// New Test Scripts
 import '../homepage/homepageCampusPartnerUser.cy'
 import '../homepage/homepageAdministrator.cy'
-import '../analytics/reports/engagementTypesReportsPublicUser.cy'
 import '../analytics/reports/engagementTypesReportsCampusPartnerUser.cy'
 import '../analytics/reports/engagementTypesReportsAdministrator.cy'
+
+// Data-cy elements added in
+import '../homepage/homepagePublicUser.cy'
+import '../analytics/reports/engagementTypesReportsPublicUser.cy'
+
+//hardcoded link update done in
+import '../registerPartners/registerCampusPartnerCampusPartnerUser.cy'
+import '../projects/myDraftCampusPartnerUser.cy'
+import '../analytics/reports/focusAreaReportsPublicUser.cy'
+import '../analytics/reports/focusAreaReportsCampusPartnerUser.cy'

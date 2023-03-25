0b07500d39aa6a1b5df072bdd5420b3c4015eb1f
shireenbaboon <shireenbaboon@gmail.com>
2023-03-25 01:15:01 -0500

    Recent progress for test script and data-cy adding

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   e2e: {
       experimentalRunAllSpecs:true,
+       watchForFileChanges:false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
         baseUrl: 'place the base URL under test'

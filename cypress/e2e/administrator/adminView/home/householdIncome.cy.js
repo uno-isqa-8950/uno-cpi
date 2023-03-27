b21131c872e69804b3c16b2dc9d06703b06c97f7
Lyra <diana.monta28@hotmail.com>
2023-03-27 01:55:57 -0500

    Adding new cypress scripts

--- cypress/e2e/administrator/adminView/home/householdIncome.cy.js
+++ cypress/e2e/administrator/adminView/home/householdIncome.cy.js
@@ -7,7 +7,7 @@ beforeEach(() => {
         }
     })
     cy.visit(Cypress.env('baseUrl'))
-
+    cy.get('#login').click().loginAdminUser(user)
 })
 
 
@@ -15,12 +15,11 @@ describe("List household income", () => {
     beforeEach(function() {
         cy.fixture("datareports").then(function(data) {
             this.data = data
-        cy.get('#login').click().loginAdminUser(user)
         })
     })
 
     const adminHref = `a[href="/admin"]`,
-        administratorLink = `a[class="nav-link dropdown-toggle"]`,
+        administratorLink = '[data-cy="administrator"]',
         adminTable = '#content-main',
         householdIncomeColumn = '.model-householdincome > th > a',
         addHouseholdIncome = `a[href="/admin/home/householdincome/add/"]`,

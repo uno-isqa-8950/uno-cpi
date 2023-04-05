a31bed25d33f4f6c428afbd5d1707e7b057b2b9c
anguyen2396 <77310745+anguyen2396@users.noreply.github.com>
2023-04-05 10:01:27 -0500

    Add audit logs

--- cypress/e2e/homepage/auditLogsAdminUser.js
+++ cypress/e2e/homepage/auditLogsAdminUser.js
@@ -3,32 +3,29 @@ import user from "../../support/commands";
 import * as data from "../../fixtures/datareports.json";
 
 beforeEach(() => {
-    cy.on('uncaught:exception', (err, runnable) => {
-        if(err.message.includes('is not a function') || err.message.includes('is not defined') || err.message.includes('reading \'addEventListener\'') || err.message.includes('reading \'style\''))
-        {
-            return false
-        }
-    })
-    cy.visit(Cypress.env('baseUrl'))
- })
-describe ('All focus area cards for admin user', () => {
+  cy.on('uncaught:exception', (err, runnable) => {
+    if(err.message.includes('is not a function') || err.message.includes('is not defined') || err.message.includes('reading \'addEventListener\'') || err.message.includes('reading \'style\''))
+    {
+      return false
+    }
+  })
+  cy.visit(Cypress.env('baseUrl'))
+})
+
+describe('Audit Logs for admin user', () => {
     beforeEach(function () {
         cy.fixture("datareports").then(function (data) {
             this.data = data
-            cy.get('#login').click()
-                .loginAdminUser(user)
+        cy.get('#login').click()
+        cy.loginCampusUser(user)
         })
     })
-
-    //verify audit logs under administrators in navigation bar.
-    it('visits audit logs', () => {
+    //Check navigation to Engagement types charts
+    it('validate audit logs exist', function () {
         cy.get('[data-cy="administrator"]').should('exist').click()
         cy.get('[data-cy="auditlogs"]').next('[data-cy="dropdown-administrator"]').then($el => {
             cy.wrap($el).invoke('show')
             cy.wrap($el).contains('Audit Logs').click()
         })
     })
-
 })
-
-
